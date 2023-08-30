679752c3942745fa79335f5126db9fa6cafd80c7
MartMbithi <martdevelopers254@gmail.com>
2023-08-30 13:12:06 +0300

    Implement Landing Scripts

---
+++ app/partials/landing_head.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Bakery Management System</title>
+    <meta name="description" content="240+ Best Bootstrap Templates are available on this website. Find your template for your project compatible with the most popular HTML library in the world." />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="canonical" href="http://127.0.0.1/bms/" />
+
+    <!-- CSS 
+    ========================= -->
+    <link rel="stylesheet" href="../public/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/slick.css">
+    <link rel="stylesheet" href="../public/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../public/css/ionicons.min.css">
+    <link rel="stylesheet" href="../public/css/pe-icon-7-stroke.css">
+    <link rel="stylesheet" href="../public/css/animate.css">
+    <link rel="stylesheet" href="../public/css/nice-select.css">
+    <link rel="stylesheet" href="../public/css/magnific-popup.css">
+    <link rel="stylesheet" href="../public/css/jquery-ui.min.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="../public/css/style.css">
+
+    <!--modernizr min js here-->
+    <script src="../public/js/vendor/modernizr-3.11.2.min.js"></script>
+
+
+    <!-- Structured Data  -->
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "",
+            "url": "Replace_with_your_site_URL"
+        }
+    </script>
+</head>
