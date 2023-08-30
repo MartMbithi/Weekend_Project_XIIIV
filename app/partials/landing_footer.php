f7a8e566c7033db7daa8390ce9e82b16ec5f6657
MartMbithi <martdevelopers254@gmail.com>
2023-08-30 13:33:31 +0300

    Implement Landing Footer

---
+++ app/partials/landing_footer.php
@@ -0,0 +1,11 @@
+<footer class="footer_widgets">
+    <div class="container">
+        <div class="footer_bottom">
+            <div class="copyright_right text-center">
+                <p>
+                     © <?php echo date('Y'); ?> All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
