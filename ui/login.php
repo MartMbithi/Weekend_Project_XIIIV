3ab03e2bdf966060087776de851502abb2e1b9f8
MartMbithi <martdevelopers254@gmail.com>
2023-09-02 10:51:54 +0300

    Implement Auth Details

---
+++ ui/login.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+require_once('../app/config/config.php');
+require_once('../app/helpers/auth.php');
+require_once('../app/partials/head.php');
+?>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#"><b>Bakery</b>Management System</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+
+                <form method="post">
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" required name="login_email" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" required name="login_password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="Login" class="btn btn-primary btn-block">
+                                Sign In
+                            </button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+                <hr>
+                <p class="mb-1">
+                    <a href="reset_password">I forgot my password</a>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+    <?php require_once('../app/partials/scripts.php'); ?>
+
+</body>
+
+
+</html>
