ad48ee5de7d1b6e111edd4702a1e38654ef6359d
MartMbithi <martdevelopers254@gmail.com>
2023-09-02 10:55:06 +0300

    Implement Reset Password - Update Auth Connections

--- app/helpers/auth.php
+++ app/helpers/auth.php
@@ -1,7 +1,7 @@
 <?php
 /* Login */
 if (isset($_POST['Login'])) {
-    $login_username = mysqli_real_escape_string($mysqli, $_POST['login_username']);
+    $login_email = mysqli_real_escape_string($mysqli, $_POST['login_email']);
     $login_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['login_password'])));
 
     /* Process login */
@@ -23,32 +23,28 @@ if (isset($_POST['Login'])) {
     }
 }
 
-/* Register Customer Account */
-if(isset($_POST['Add_Customer_Account'])){
-    
-}
-
 
 /* Reset Password Step 1 */
 if (isset($_POST['Reset_Password_1'])) {
-    $login_username  = mysqli_real_escape_string($mysqli, $_POST['login_username']);
+    $login_email  = mysqli_real_escape_string($mysqli, $_POST['login_email']);
 
     /* Check If Login Username Exists */
-    $username_checker = "SELECT * FROM login WHERE login_email = '{$login_username}'";
+    $username_checker = "SELECT * FROM login WHERE login_email = '{$login_email}'";
     $res = mysqli_query($mysqli, $username_checker);
     if (mysqli_num_rows($res) > 0) {
         /* Redirect to reset password step 2 */
+        $_SESSION['login_email'] = $login_email;
         $_SESSION['success'] = "Login username verified, proceed to reset password";
         header('Location: confirm_password');
         exit;
     } else {
-        $err = "Login username does not exist";
+        $err = "Login email does not exist";
     }
 }
 
 /* Reset Password Step 2 */
 if (isset($_POST['Reset_Password_Step_2'])) {
-    $login_username = mysqli_real_escape_string($mysqli, $_SESSION['login_username']);
+    $login_email = mysqli_real_escape_string($mysqli, $_SESSION['login_email']);
     $new_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['new_password'])));
     $confirm_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['confirm_password'])));
 
@@ -57,7 +53,7 @@ if (isset($_POST['Reset_Password_Step_2'])) {
         $err = "Passwords does not match";
     } else {
         /* Persist */
-        $update_passwords = "UPDATE login SET login_password = '{$confirm_password}' WHERE login_email = '{$login_username}'";
+        $update_passwords = "UPDATE login SET login_password = '{$confirm_password}' WHERE login_email = '{$login_email}'";
 
         if (mysqli_query($mysqli, $update_passwords)) {
             /* Redirect to login */
