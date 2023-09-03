d81b5c6ebd613d8fecf9f9432ead77ddfa6b4495
MartMbithi <martdevelopers254@gmail.com>
2023-09-03 10:20:10 +0300

    Implement Staffs Analytics

--- app/functions/analytics.php
+++ app/functions/analytics.php
@@ -38,6 +38,45 @@ if ($login_rank == '1') {
     $stmt->close();
 
 
+    /* Total Orders */
+    $query = "SELECT COUNT(*) FROM orders";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($total_orders);
+    $stmt->fetch();
+    $stmt->close();
+
+
+    /* Income */
+    $query = "SELECT SUM(order_price) FROM orders WHERE order_status = 'Paid'";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($payments);
+    $stmt->fetch();
+    $stmt->close();
+} else {
+    /* Staffs */
+
+
+
+    /* Customers */
+    $query = "SELECT COUNT(*) FROM customers";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($customers);
+    $stmt->fetch();
+    $stmt->close();
+
+
+    /* Products */
+    $query = "SELECT COUNT(*) FROM products";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($products);
+    $stmt->fetch();
+    $stmt->close();
+
+
     /* Orders */
     $query = "SELECT COUNT(*) FROM orders";
     $stmt = $mysqli->prepare($query);
@@ -47,7 +86,11 @@ if ($login_rank == '1') {
     $stmt->close();
 
 
-
-} else {
-    /* Customer Analytics */
+    /* Income */
+    $query = "SELECT SUM(order_price) FROM orders WHERE order_status = 'Paid'";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($payments);
+    $stmt->fetch();
+    $stmt->close();
 }
