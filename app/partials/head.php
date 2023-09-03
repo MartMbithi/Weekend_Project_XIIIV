ab77796d0d47fdb4209b9812901a940c26097dc9
MartMbithi <martdevelopers254@gmail.com>
2023-09-03 10:57:20 +0300

    Implement Dashboard - Install Data Table Plugins

--- app/partials/head.php
+++ app/partials/head.php
@@ -24,6 +24,11 @@
     <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <!-- Toastr -->
     <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/toastr/toastr.min.css">
+    <!-- Data tables Buttons -->
+    <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <?php
     /* Alert Sesion Via Alerts */
     if (isset($_SESSION['success'])) {
