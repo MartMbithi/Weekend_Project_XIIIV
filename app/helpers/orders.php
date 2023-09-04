2ded5cf705d56d212c7d6926442f5aa0bee9b941
MartMbithi <martdevelopers254@gmail.com>
2023-09-04 13:11:58 +0300

    Implement Orders Details

--- app/helpers/orders.php
+++ app/helpers/orders.php
@@ -1,5 +1,21 @@
 <?php
 /*Update Orders */
+if (isset($_POST['Update_Orders'])) {
+    $order_id = mysqli_real_escape_string($mysqli, $_POST['order_id']);
+    $order_qty = mysqli_real_escape_string($mysqli, $_POST['order_qty']);
+    $order_product_id = mysqli_real_escape_string($mysqli, $_POST['order_product_id']);
+    $order_status = mysqli_real_escape_string($mysqli, $_POST['order_status']);
+
+    /* Roll Back */
+    $roll_back_product = "UPDATE products SET product_qty = product_qty + '{$order_qty}' WHERE product_id = '{$order_product_id}'";
+    $update_order = "UPDATE orders SET order_qty = '{$order_qty}', order_status = '{$order_status}' WHERE order_id = '{$order_id}'";
+
+    if (mysqli_query($mysqli, $update_order) && mysqli_query($mysqli, $roll_back_product)) {
+        $success = "Order updated";
+    } else {
+        $err = "Failed, Please try again";
+    }
+}
 
 /* Delete Orders */
 if (isset($_POST['Delete_Orders'])) {
