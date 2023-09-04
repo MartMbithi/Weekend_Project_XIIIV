d2a16813a6d6b4189e69b8a04b7103b48e553599
MartMbithi <martdevelopers254@gmail.com>
2023-09-04 15:10:59 +0300

    Implement Orders Roll Back

--- app/helpers/orders.php
+++ app/helpers/orders.php
@@ -4,11 +4,10 @@ if (isset($_POST['Update_Orders'])) {
     $order_id = mysqli_real_escape_string($mysqli, $_POST['order_id']);
     $order_qty = mysqli_real_escape_string($mysqli, $_POST['order_qty']);
     $order_product_id = mysqli_real_escape_string($mysqli, $_POST['order_product_id']);
-    $order_status = mysqli_real_escape_string($mysqli, $_POST['order_status']);
 
     /* Roll Back */
     $roll_back_product = "UPDATE products SET product_available_qty = product_available_qty + '{$order_qty}' WHERE product_id = '{$order_product_id}'";
-    $update_order = "UPDATE orders SET order_qty = '{$order_qty}', order_status = '{$order_status}' WHERE order_id = '{$order_id}'";
+    $update_order = "UPDATE orders SET order_qty = '{$order_qty}' WHERE order_id = '{$order_id}'";
 
     if (mysqli_query($mysqli, $update_order) && mysqli_query($mysqli, $roll_back_product)) {
         $success = "Order updated";
--- app/modals/orders.php
+++ app/modals/orders.php
@@ -1,6 +1,41 @@
 <!-- Pay Orders -->
 
 <!-- Edit Orders -->
+<div class="modal fade fixed-right" id="update_<?php echo $orders['order_id']; ?>" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered  modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header align-items-center">
+                <div class="text-center">
+                    <h6 class="mb-0 text-bold">Update order</h6>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="">Order QTY</label>
+                            <input type="hidden" required name="order_product_id" value="<?php echo $orders['order_product_id']; ?>" class="form-control">
+                            <input type="text" required name="order_qty" value="<?php echo $orders['order_qty']; ?>" class="form-control">
+                            <input type="hidden" required name="order_id" value="<?php echo $orders['order_id']; ?>" class="form-control">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="">Date</label>
+                            <input type="date" required name="order_date" value="<?php echo $orders['order_date']; ?>" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="text-right">
+                        <button type="submit" name="Update_Orders" class="btn btn-outline-success">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <!-- Delete Orders -->
 <div class="modal fade" id="delete_<?php echo $orders['order_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
