3bebda06a29c3ed9b6b24e47ecd52fe3d652d516
MartMbithi <martdevelopers254@gmail.com>
2023-09-04 13:06:36 +0300

    Implement Orders  - Payment Details

--- app/helpers/orders.php
+++ app/helpers/orders.php
@@ -1,2 +1,19 @@
 <?php
-/* Add Orders */
+/*Update Orders */
+
+/* Delete Orders */
+
+/* Pay Orders */
+if (isset($_POST['Pay_Order'])) {
+    $order_id = mysqli_real_escape_string($mysqli, $_POST['order_id']);
+    $order_status = mysqli_real_escape_string($mysqli, $_POST['order_status']);
+
+    /* Payment */
+    $add_payment = "UPDATE orders SET order_status = '{$order_status}' WHERE order_id = '{$order_id}'";
+
+    if (mysqli_query($mysqli, $add_payment)) {
+        $success = "Payment Successful";
+    } else {
+        $err = "Failed, please try again";
+    }
+}
