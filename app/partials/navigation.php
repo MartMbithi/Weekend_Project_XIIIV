1a2f0a6b176314c25e4cf6425587609069fdcd3b
MartMbithi <martdevelopers254@gmail.com>
2023-09-05 05:21:42 +0300

    Craft Non Admin Access Level

--- app/partials/navigation.php
+++ app/partials/navigation.php
@@ -8,39 +8,73 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="dashboard" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="staffs" class="nav-link">Staffs</a>
-                </li>
-                <li class="nav-item">
-                    <a href="customers" class="nav-link">Customers</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Products</a>
-                    <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                        <li><a href="categories" class="dropdown-item">Categories </a></li>
-                        <li><a href="products" class="dropdown-item">Products</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="orders" class="nav-link">Orders</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Reports</a>
-                    <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                        <li><a href="reports_staffs" class="dropdown-item">Staffs </a></li>
-                        <li><a href="reports_customers" class="dropdown-item">Customers</a></li>
-                        <li><a href="reports_inventory" class="dropdown-item">Inventory</a></li>
-                        <li><a href="reports_orders" class="dropdown-item">Orders</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+        <?php if ($user_access_level == '1') { ?>
+
+            <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="dashboard" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="staffs" class="nav-link">Staffs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="customers" class="nav-link">Customers</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Products</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="categories" class="dropdown-item">Categories </a></li>
+                            <li><a href="products" class="dropdown-item">Products</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="orders" class="nav-link">Orders</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Reports</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="reports_staffs" class="dropdown-item">Staffs </a></li>
+                            <li><a href="reports_customers" class="dropdown-item">Customers</a></li>
+                            <li><a href="reports_inventory" class="dropdown-item">Inventory</a></li>
+                            <li><a href="reports_orders" class="dropdown-item">Orders</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        <?php } else { ?>
+
+            <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="dashboard" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="customers" class="nav-link">Customers</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Products</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="categories" class="dropdown-item">Categories </a></li>
+                            <li><a href="products" class="dropdown-item">Products</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="orders" class="nav-link">Orders</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Reports</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="reports_customers" class="dropdown-item">Customers</a></li>
+                            <li><a href="reports_inventory" class="dropdown-item">Inventory</a></li>
+                            <li><a href="reports_orders" class="dropdown-item">Orders</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        <?php } ?>
 
         <!-- Right navbar links -->
         <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
