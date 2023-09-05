905965333710d71de72a2cd3a40cd81df1973a8e
MartMbithi <martdevelopers254@gmail.com>
2023-09-05 05:51:00 +0300

    Implement Profile Pages - All Unit Tests Pass

--- ui/profile.php
+++ ui/profile.php
@@ -52,7 +52,7 @@ require_once('../app/partials/head.php');
                                     <div class="card card-success card-outline">
                                         <div class="card-body box-profile">
                                             <div class="text-center">
-                                                <img class="profile-user-img img-fluid img-circle" src="../public/img/avatar.png" alt="User profile picture">
+                                                <img class="profile-user-img img-fluid img-circle" src="../public/img/no-profile.png" alt="User profile picture">
                                             </div>
 
                                             <h3 class="profile-username text-center"><?php echo $user['user_name']; ?></h3>
@@ -95,6 +95,7 @@ require_once('../app/partials/head.php');
                                                                 <div class="row">
                                                                     <div class="form-group col-md-12">
                                                                         <label for="">Full names</label>
+                                                                        <input type="hidden" required name="login_rank" value="<?php echo $user['login_rank']; ?>" class="form-control">
                                                                         <input type="hidden" required name="user_login_id" value="<?php echo $user['user_login_id']; ?>" class="form-control">
                                                                         <input type="text" required name="user_name" value="<?php echo $user['user_name']; ?>" class="form-control">
                                                                     </div>
@@ -112,7 +113,7 @@ require_once('../app/partials/head.php');
                                                                     </div>
                                                                 </div>
                                                                 <div class="text-right">
-                                                                    <button type="submit" name="Update_Personal_Details" class="btn btn-outline-success">Save</button>
+                                                                    <button type="submit" name="Update_Staff" class="btn btn-outline-success">Save</button>
                                                                 </div>
                                                             </form>
                                                             <!-- /.card-body -->
@@ -124,21 +125,18 @@ require_once('../app/partials/head.php');
                                                             <div class="card-body">
                                                                 <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
                                                                     <div class="row">
-                                                                        <div class="form-group col-md-6">
-                                                                            <label for="">Old password</label>
-                                                                            <input type="password" required name="old_password" class="form-control">
-                                                                        </div>
-                                                                        <div class="form-group col-md-6">
+                                                                        <div class="form-group col-md-12">
                                                                             <label for="">New password</label>
-                                                                            <input type="password" required name="new_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" class="form-control">
+                                                                            <input type="hidden" required name="user_login_id" value="<?php echo $user['user_login_id']; ?>" class="form-control">
+                                                                            <input type="password" required name="new_password" class="form-control">
                                                                         </div>
                                                                         <div class="form-group col-md-12">
                                                                             <label for="">Confirm password</label>
-                                                                            <input type="password" required name="confirm_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" class="form-control">
+                                                                            <input type="password" required name="confirm_password" class="form-control">
                                                                         </div>
                                                                     </div>
                                                                     <div class="text-right">
-                                                                        <button type="submit" name="Update_Personal_Password" class="btn btn-outline-success">Save</button>
+                                                                        <button type="submit" name="Change_Password" class="btn btn-outline-success">Save</button>
                                                                     </div>
                                                                 </form>
                                                             </div>
